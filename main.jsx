5cfb763af0b9020cd8eb2ed54a3ebb57c22fcfd7
pnr62nvzbn-ship-it <pnr62nvzbn@privaterelay.appleid.com>
2026-08-16 21:11:12 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WorkoutLogger from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WorkoutLogger />
+  </React.StrictMode>
+);
